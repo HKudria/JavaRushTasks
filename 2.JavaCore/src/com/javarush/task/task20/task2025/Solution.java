20eed3ccd669aae64b275d5f381a2d667ae9579d
Herman Kudria <h.kudrya@hotmail.com>
2021-03-18 19:06:13 +0100

    task 20.25

--- 2.JavaCore/src/com/javarush/task/task20/task2025/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2025/Solution.java
@@ -13,33 +13,39 @@ public class Solution {
 
     public static long[] getNumbers(long N) {
 
-        ArrayList<Long> l = new ArrayList<>();
-        for (int m = 0; m < N; m++) {
+       /* ArrayList<Long> l = new ArrayList<>();
+        for (long m = 1; m < N; m++) {
             String number = m + "";
             long bg = 0;
-            for (int i = 0; i < number.length(); i++) {
-                long sd = Long.parseLong(String.valueOf(number.charAt(i)));
+            for (long i = 0; i < number.length(); i++) {
+                long sd = Long.parseLong(String.valueOf(number.charAt(Math.toIntExact(i))));
 
                 long s = 1;
                 if (sd != 0) {
-                    for (int j = 0; j < number.length(); j++) {
+                    for (long j = 0; j < number.length(); j++) {
                         s *= sd;
                     }
                 } else s = 0;
                 bg +=s;
             }
-           if (bg<N && bg == Long.parseLong(number)) l.add(bg);
+           if (bg<N && bg == Long.parseLong(number)) {
+               l.add(bg);
+               //System.out.println(bg);
+           }
         }
         long[] result = new long[l.size()];
         for (int i = 0; i < l.size(); i++) {
             result[i]=l.get(i);
         }
         Arrays.sort(result);
-        return result;
+
+        */
+
+        return null;
     }
 
     public static void main(String[] args) {
-        long a = System.currentTimeMillis();
+      long a = System.currentTimeMillis();
         System.out.println(Arrays.toString(getNumbers(1000)));
         long b = System.currentTimeMillis();
         System.out.println("memory " + (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / (8 * 1024));
