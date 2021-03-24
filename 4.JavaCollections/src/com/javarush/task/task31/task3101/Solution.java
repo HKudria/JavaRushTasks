c5f2c5dc422781b640647627374bfb1f3b4afd66
Herman Kudria <h.kudrya@hotmail.com>
2021-03-24 17:45:03 +0100

    task 3101

---
+++ 4.JavaCollections/src/com/javarush/task/task31/task3101/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task31.task3101;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.EnumSet;
+import java.util.Map;
+import java.util.TreeMap;
+
+/* 
+Проход по дереву файлов
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        args = new String[]{"D:/test/","D:/test/resultFileAbsolutePath.txt"};
+        File folder = new File(args[0]);
+        File result = new File(args[1]);
+        result.renameTo()
+
+    }
+}
