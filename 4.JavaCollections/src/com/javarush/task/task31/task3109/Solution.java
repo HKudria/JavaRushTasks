5655cc800cb26103a1debe3e2551f76f7ee1daf4
Herman Kudria <h.kudrya@hotmail.com>
2021-04-07 17:30:43 +0200

    task 3109

---
+++ 4.JavaCollections/src/com/javarush/task/task31/task3109/Solution.java
@@ -0,0 +1,42 @@
+package com.javarush.task.task31.task3109;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Properties;
+
+/* 
+Читаем конфиги
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        Properties properties = solution.getProperties("4.JavaCollections/src/com/javarush/task/task31/task3109/properties.xml");
+        properties.list(System.out);
+
+        properties = solution.getProperties("4.JavaCollections/src/com/javarush/task/task31/task3109/properties.txt");
+        properties.list(System.out);
+
+        properties = solution.getProperties("4.JavaCollections/src/com/javarush/task/task31/task3109/notExists");
+        properties.list(System.out);
+    }
+
+    public Properties getProperties(String fileName) {
+
+        Properties properties = new Properties();
+        try {
+            Path pr = Paths.get(fileName);
+            if (pr.toString().endsWith(".xml")) {
+                properties.loadFromXML(new FileInputStream(pr.toFile()));
+            } else {
+                properties.load(new FileInputStream(pr.toFile()));
+            }
+            return properties;
+        } catch (Exception e){
+            return properties;
+        }
+
+    }
+}
