430d229a4411f3801134e88f1f7ce6cc87f635b3
Herman Kudria <h.kudrya@hotmail.com>
2021-07-25 12:05:45 +0200

    Java Multithreading Level 24 24.05 I don't understand ------

---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2401/SelfInterfaceMarkerImpl.java
@@ -0,0 +1,11 @@
+package com.javarush.task.task24.task2401;
+
+public class SelfInterfaceMarkerImpl implements SelfInterfaceMarker{
+    public void First (){
+        System.out.println("Hello ");
+    }
+
+    public void Second () {
+        System.out.println("world!");
+    }
+}
